59435c3aebf53131bdabf8ce9af214e7c7858369
Joshgiku17 <joshuagikundiro17@gmail.com>
2023-05-15 18:02:14 +0200

    second commit

--- api.js
+++ api.js
@@ -3,7 +3,8 @@ const app = express();
 const userRoutes = require('./routes/node');
 
 app.use('/node', userRoutes);
+const {createUser, getUserById,updateUser,deleteUser} =require('./routes/node');
 app.use(express.static('views'));
 app.listen(3000, () => {
   console.log('Server started on port 3000');
-});
+});remot
--- routes/node.js
+++ routes/node.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
 const userController = require('../controller/userController');
 
-module.exports = (pool) =>{
+module.exports = (pool) => {
+    
+    // Create a new user
+    router.post('/create', userController.createUser);
 
+    // Get a user by ID
+    router.get('/:userId', userController.getUserById);
 
-// Create a new user
-router.post('/create', userController.createUser);
+    // Update a user by ID
+    router.put('/:userId', userController.updateUser);
 
-// Get a user by ID
-router.get('/:userId', userController.getUserById);
+    // Delete a user by ID
+    router.delete('/:userId', userController.deleteUser);
 
-// Update a user by ID
-router.put('/:userId', userController.updateUser);
-
-// Delete a user by ID
-router.delete('/:userId', userController.deleteUser);
-
-return router;
+    return router;
 }
 
